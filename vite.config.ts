ee5273e67621000b29e960acb9b6af53a9f695a0
phuong <92218405+phuonglat142@users.noreply.github.com>
2025-09-15 08:40:25 +0700

    ui layout register

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000,
+    port: 3000
   },
   css: {
-    devSourcemap: true,
+    devSourcemap: true
   },
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+      '~': path.resolve(__dirname, './src')
+    }
+  }
+})
